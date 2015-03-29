aea34acbc483d5e72920b7512a3431d55761b618
ColtonGerhart <coltonpsu@gmail.com>
2015-03-29 12:56:05 -0400

    Sunday Work
    Worked on the Prospective Students page mostly, but then some small
    changes to the banner and navigation menu.

---
+++ layout/scripts/prospectiveStudents.js
@@ -0,0 +1,42 @@
+﻿function mathUndergradClick(){
+    if ($("#prospectiveMathUndergraduate").is(':visible')) {
+        $("#prospectiveMathUndergraduate").hide();
+        $("#prospectiveCSUndergraduate").hide();
+        $("#prospectiveCSGraduate").hide();
+    }
+    else {
+        $("#prospectiveMathUndergraduate").show();
+        $("#prospectiveCSUndergraduate").hide();
+        $("#prospectiveCSGraduate").hide();
+    }
+}
+
+function csUndergradClick() {
+    if ($("#prospectiveCSUndergraduate").is(':visible')) {
+        $("#prospectiveMathUndergraduate").hide();
+        $("#prospectiveCSUndergraduate").hide();
+        $("#prospectiveCSGraduate").hide();
+    }
+    else {
+        $("#prospectiveMathUndergraduate").hide();
+        $("#prospectiveCSUndergraduate").show();
+        $("#prospectiveCSGraduate").hide();
+    }
+}
+
+function csGradClick() {
+    if ($("#prospectiveCSGraduate").is(':visible')) {
+        $("#prospectiveMathUndergraduate").hide();
+        $("#prospectiveCSUndergraduate").hide();
+        $("#prospectiveCSGraduate").hide();
+    }
+    else {
+        $("#prospectiveMathUndergraduate").hide();
+        $("#prospectiveCSUndergraduate").hide();
+        $("#prospectiveCSGraduate").show();
+    }
+}
+
+function loadDiv(url) {
+    $('#content').load(url);
+}
